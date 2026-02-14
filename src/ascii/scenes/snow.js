88bd6e65bedaeb142eed4a6ad74eb3ab4dbf5e4a
Justin Elrod <justinelrod111@gmail.com>
2026-02-13 18:51:55 -0800

    Add animated snow and thunder ASCII art
    - Snow: 4-frame animation with falling snowflakes
    - Thunder: 4-frame animation with lightning flashes
    - All frames within width 55

--- src/ascii/scenes/snow.js
+++ src/ascii/scenes/snow.js
@@ -1,9 +1,12 @@
-const art = [
+// Snow animation frames - falling snowflakes
+// 4 frames with subtle snow position shifts (all within width 55)
+
+const snowFrame0 = [
   '            .-~~~-.                                  ',
   '      .- ~ ~-(       )- ~       .-~~~-.             ',
   '     /                     \\.- ~-(       )- ~      ',
-  '    |                       |                  \\   ',
-  '     \\                     / \\                /   ',
+  '    |   * . * . * .   |                  \\   ',
+  '     \\   . * . * .  / \\                /   ',
   '       ~- . _____ . -~       ~- . ___ . -~         ',
   '     *    .   *    .   *   .    *    .   *          ',
   '       .    *   .( _ _._   .    *  .    .           ',
@@ -14,10 +17,60 @@ const art = [
   '   |_________|___|__________|=|=|=|=|=|              '
 ];
 
+const snowFrame1 = [
+  '            .-~~~-.                                  ',
+  '      .- ~ ~-(       )- ~       .-~~~-.             ',
+  '     /                     \\.- ~-(       )- ~      ',
+  '    |                       |   * . * . * \\   ',
+  '     \\    * . * .   / \\                /   ',
+  '       ~- . _____ . -~       ~- . ___ . -~         ',
+  '       .    *   .   *   .    *   .    *             ',
+  '     *   .    *( _ _._   .    *  .    *           ',
+  "       .   * |_|-'_~_`-._   *    .  *            ",
+  "     *   .-'-_~_-~_-~-_`-._  .    *   .       ",
+  '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
+  '   | []  []   ___   []  [] |_._._._._.               ',
+  '   |_________|___|__________|=|=|=|=|=|              '
+];
+
+const snowFrame2 = [
+  '            .-~~~-.                                  ',
+  '      .- ~ ~-(       )- ~       .-~~~-.             ',
+  '     /                     \\.- ~-(       )- ~      ',
+  '    |   * . * . * .   |                  \\   ',
+  '     \\   . * . * .  / \\                /   ',
+  '       ~- . _____ . -~       ~- . ___ . -~         ',
+  '     *    .   *    .   *   .    *    .   *          ',
+  '       .    *   .( _ _._   .    *  .    .           ',
+  "     *   .   * |_|-'_~_`-._   *    .  *            ",
+  "       .   * .-'-_~_-~_-~-_`-._  .    *   .       ",
+  '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
+  '   | []  []   ___   []  [] |_._._._._.               ',
+  '   |_________|___|__________|=|=|=|=|=|              '
+];
+
+const snowFrame3 = [
+  '            .-~~~-.                                  ',
+  '      .- ~ ~-(       )- ~       .-~~~-.             ',
+  '     /                     \\.- ~-(       )- ~      ',
+  '    |                       |   * . * . * \\   ',
+  '     \\    * . * .   / \\                /   ',
+  '       ~- . _____ . -~       ~- . ___ . -~         ',
+  '       .    *   .   *   .    *   .    *             ',
+  '     *   .    *( _ _._   .    *  .    *           ',
+  "       .   * |_|-'_~_`-._   *    .  *            ",
+  "     *   .-'-_~_-~_-~-_`-._  .    *   .       ",
+  '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
+  '   | []  []   ___   []  [] |_._._._._.               ',
+  '   |_________|___|__________|=|=|=|=|=|              '
+];
+
+const frames = [snowFrame0, snowFrame1, snowFrame2, snowFrame3];
+
 export default {
   name: 'snow',
   width: 55,
-  height: art.length,
+  height: snowFrame0.length,
   defaultColor: 'cloud',
   charColors: {
     '*': 'snow',
@@ -35,7 +88,15 @@ export default {
     '|': 'houseWall',
     "'": 'houseWall'
   },
-  getArt() {
-    return art;
-  }
+  // Return all frames for animation
+  getFrames() {
+    return frames;
+  },
+  // Return single frame for static display
+  // Accepts frameIndex (number) or options object for backwards compatibility
+  getArt(frameIndexOrOptions = 0) {
+    const frameIndex = typeof frameIndexOrOptions === 'number' ? frameIndexOrOptions : 0;
+    return frames[frameIndex % frames.length];
+  },
+  frameCount: frames.length
 };
