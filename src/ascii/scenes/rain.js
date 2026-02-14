1a172b8e847507730b34f64054aec332ce127916
Justin D Elrod <31932854+deephouse23@users.noreply.github.com>
2026-02-13 18:49:56 -0800

    Add animated rain ASCII art (#29)
    * Add animated rain ASCII art with multi-frame animation
    
    - Convert rain scene to 4-frame animation with falling rain effect
    - Add backwards compatibility for getArt(options) call
    - Update snapshot for rain scene
    
    * Fix: ensure rain animation frames stay within width 55
    
    - Adjust frame content to not exceed scene width
    - Prevents animation jitter and layout issues
    
    * chore: trigger bugbot re-review

--- src/ascii/scenes/rain.js
+++ src/ascii/scenes/rain.js
@@ -1,12 +1,15 @@
-const art = [
+// Rain animation frames - falling rain drops
+// 4 frames with subtle rain position shifts (all frames within width 55)
+
+const rainFrame0 = [
   '            .-~~~-.                                  ',
   '      .- ~ ~-(       )- ~       .-~~~-.             ',
   '     /                     \\.- ~-(       )- ~      ',
-  '    |                       |                  \\   ',
-  '     \\                     / \\                /   ',
+  '    |   , | , |  , | ,   |                  \\   ',
+  '     \\   , |  ,|    / \\                /   ',
   '       ~- . _____ . -~       ~- . ___ . -~         ',
-  '      ,  |  ,  |  ,  |  , |  ,  |  ,  |  ,         ',
-  '    |  ,  |  ,(  _ _._  |  ,  |  ,  |  ,           ',
+  '      |  ,  |  , | , |  , |  ,  |  ,  |          ',
+  '    |  ,  |  ,(  _ _._  |  ,  |  ,  |           ',
   "      ,  |  , |_|-'_~_`-._  ,  |  ,  |             ",
   "    |  ,  |.-'-_~_-~_-~-_`-._  |  ,  |  ,         ",
   '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
@@ -14,10 +17,60 @@ const art = [
   '   |_________|___|__________|=|=|=|=|=|              '
 ];
 
+const rainFrame1 = [
+  '            .-~~~-.                                  ',
+  '      .- ~ ~-(       )- ~       .-~~~-.             ',
+  '     /                     \\.- ~-(       )- ~      ',
+  '    |                       |   , | , |  , \\   ',
+  '     \\    , | , |  ,   / \\                /   ',
+  '       ~- . _____ . -~       ~- . ___ . -~         ',
+  '      ,  |  , |  , | , |  , |  ,  |  ,  |         ',
+  '    |  ,  |  ,(  _ _._  |  ,  |  ,  |           ',
+  "      ,  |  , |_|-'_~_`-._  ,  |  ,  |             ",
+  "    |  ,  |.-'-_~_-~_-~-_`-._  |  ,  |  ,         ",
+  '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
+  '   | []  []   ___   []  [] |_._._._._.               ',
+  '   |_________|___|__________|=|=|=|=|=|              '
+];
+
+const rainFrame2 = [
+  '            .-~~~-.                                  ',
+  '      .- ~ ~-(       )- ~       .-~~~-.             ',
+  '     /                     \\.- ~-(       )- ~      ',
+  '    |                       |                  \\   ',
+  '     \\ , | , |  , | ,   / \\                /   ',
+  '       ~- . _____ . -~       ~- . ___ . -~         ',
+  '      ,  |  ,  |  , | , |  , |  ,  |  ,  |         ',
+  '    |  ,  |  ,(  _ _._  |  ,  |  ,  |           ',
+  "      ,  |  , |_|-'_~_`-._  ,  |  ,  |             ",
+  "    |  ,  |.-'-_~_-~_-~-_`-._  |  ,  |  ,         ",
+  '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
+  '   | []  []   ___   []  [] |_._._._._.               ',
+  '   |_________|___|__________|=|=|=|=|=|              '
+];
+
+const rainFrame3 = [
+  '            .-~~~-.                                  ',
+  '      .- ~ ~-(       )- ~       .-~~~-.             ',
+  '     /                     \\.- ~-(       )- ~      ',
+  '    |   , | , |  , | ,    |  |                  \\   ',
+  '     \\    ,  |  ,|    / \\                /   ',
+  '       ~- . _____ . -~       ~- . ___ . -~         ',
+  '      ,  |  ,  |  ,  | , |  , |  ,  |  ,         ',
+  '    |  ,  |  ,(  _ _._  |  ,  |  ,  |  ,           ',
+  "      ,  |  , |_|-'_~_`-._  ,  |  ,  |  ,             ",
+  "    |  ,  |.-'-_~_-~_-~-_`-._  |  ,  |  ,         ",
+  '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
+  '   | []  []   ___   []  [] |_._._._._.               ',
+  '   |_________|___|__________|=|=|=|=|=|              '
+];
+
+const frames = [rainFrame0, rainFrame1, rainFrame2, rainFrame3];
+
 export default {
   name: 'rain',
   width: 55,
-  height: art.length,
+  height: rainFrame0.length,
   defaultColor: 'cloudDark',
   charColors: {
     '|': 'rain',
@@ -35,7 +88,15 @@ export default {
     '=': 'houseDoor',
     "'": 'houseWall'
   },
-  getArt() {
-    return art;
-  }
+  // Return all frames for animation
+  getFrames() {
+    return frames;
+  },
+  // Return single frame for static display
+  // Accepts frameIndex (number) or options object for backwards compatibility
+  getArt(frameIndexOrOptions = 0) {
+    const frameIndex = typeof frameIndexOrOptions === 'number' ? frameIndexOrOptions : 0;
+    return frames[frameIndex % frames.length];
+  },
+  frameCount: frames.length
 };
