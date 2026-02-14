eb70a930951552734b897c77eab24da51bbbf48c
Justin D Elrod <31932854+deephouse23@users.noreply.github.com>
2026-02-13 19:53:42 -0800

    Add animated snow and thunder ASCII art (#30)

--- src/ascii/scenes/thunder.js
+++ src/ascii/scenes/thunder.js
@@ -1,4 +1,7 @@
-const art = [
+// Thunder animation frames - lightning flashes
+// 4 frames: alternating lightning intensity (all within width 55)
+
+const thunderFrame0 = [
   '            .-~~~-.                                  ',
   '      .- ~ ~-(       )- ~       .-~~~-.             ',
   '     /            /\\      \\.- ~-(       )- ~      ',
@@ -14,10 +17,60 @@ const art = [
   '   |_________|___|__________|=|=|=|=|=|              '
 ];
 
+const thunderFrame1 = [
+  '            .-~~~-.                                  ',
+  '      .- ~ ~-(       )- ~       .-~~~-.             ',
+  '     /            /\\      \\.- ~-(       )- ~      ',
+  '    |           ** \\ \\      |                  \\  ',
+  '     \\          *  / /     / \\                /    ',
+  '       ~- . *_***__/ /. -~    ~- . ___ . -~          ',
+  '      ,  |  , / /*  |  , |  ,  |  ,  |  ,           ',
+  '    |  ,  |  ** ,(  _ _._  |  ,  |  ,  |           ',
+  "      ,  |  ,  |_|-'_~_`-._  ,  |  ,  |           ",
+  "    |  ,  |.-'-_~_-~_-~-_`-._  |  ,  |  ,         ",
+  '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
+  '   | []  []   ___   []  [] |_._._._._.               ',
+  '   |_________|___|__________|=|=|=|=|=|              '
+];
+
+const thunderFrame2 = [
+  '            .-~~~-.                                  ',
+  '      .- ~ ~-(       )- ~       .-~~~-.             ',
+  '     /            /\\      \\.- ~-(       )- ~      ',
+  '    |             \\ \\      |                  \\  ',
+  '     \\            / /     / \\                /    ',
+  '       ~- . _____/ /. -~    ~- . ___ . -~          ',
+  '      ,  |  , / /  |  , |  ,  |  ,  |  ,           ',
+  '    |  ,  |  *  ,(  _ _._  |  ,  |  ,  |           ',
+  "      ,  |  ,  |_|-'_~_`-._  ,  |  ,  |           ",
+  "    |  ,  |.-'-_~_-~_-~-_`-._  |  ,  |  ,         ",
+  '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
+  '   | []  []   ___   []  [] |_._._._._.               ',
+  '   |_________|___|__________|=|=|=|=|=|              '
+];
+
+const thunderFrame3 = [
+  '            .-~~~-.                                  ',
+  '      .- ~ ~-(       )- ~       .-~~~-.             ',
+  '     /            /\\      \\.- ~-(       )- ~      ',
+  '    |           ** \\ \\      |                  \\  ',
+  '     \\          *  / /     / \\                /    ',
+  '       ~- . *_***__/ /. -~    ~- . ___ . -~          ',
+  '      ,  |  , / /*  |  , |  ,  |  ,  |  ,           ',
+  '    |  ,  |  ** ,(  _ _._  |  ,  |  ,  |           ',
+  "      ,  |  ,  |_|-'_~_`-._  ,  |  ,  |           ",
+  "    |  ,  |.-'-_~_-~_-~-_`-._  |  ,  |  ,         ",
+  '   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                     ',
+  '   | []  []   ___   []  [] |_._._._._.               ',
+  '   |_________|___|__________|=|=|=|=|=|              '
+];
+
+const frames = [thunderFrame0, thunderFrame1, thunderFrame2, thunderFrame3];
+
 export default {
   name: 'thunder',
   width: 55,
-  height: art.length,
+  height: thunderFrame0.length,
   defaultColor: 'cloudDark',
   charColors: {
     '/': 'lightning',
@@ -36,7 +89,15 @@ export default {
     '=': 'houseDoor',
     "'": 'houseWall'
   },
-  getArt() {
-    return art;
-  }
+  // Return all frames for animation
+  getFrames() {
+    return frames;
+  },
+  // Return single frame for static display
+  // Accepts frameIndex (number) or options object for backwards compatibility
+  getArt(frameIndexOrOptions = 0) {
+    const frameIndex = typeof frameIndexOrOptions === 'number' ? frameIndexOrOptions : 0;
+    return frames[frameIndex % frames.length];
+  },
+  frameCount: frames.length
 };
